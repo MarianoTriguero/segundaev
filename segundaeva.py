618940b83d3c66e02bd8e053fa3bdc0842ae4d9c
Mariano Triguero <mariano2455@hotmail.com>
2015-02-23 14:29:12 +0100

    a

--- segundaeva.py
+++ segundaeva.py
@@ -9,7 +9,7 @@ raiz = doc.getroot()
 localidad = raw_input("Dame el nombre de una localidad:  ").upper()
 locales = raiz.findall("row")
 for local in locales:
-	if local.find("municipality").text == localidad:
+	if local.find("locality").text == localidad:
 		print str(local.find("documentName").text) + " - " + str(local.find("phoneNumber").text) + " - " + str(local.find("email").text)
 
 print " ----------------------------------------------------------------------- "
@@ -39,23 +39,25 @@ for local in locales:
 #	Si no está la localidad en localidades se agrega a la lista
 	resp = False
 	for loc in localidades:
-		if local.find("municipality").text == loc[0]:
+		if local.find("locality").text == loc[0]:
 			resp = True
 			#Si resulta que la ciudad está en la lista, accedemos a su variable y la aumentamos en uno
 			if local.find("michelinStar").text != None:
 				loc[1] = loc[1] + int(local.find("michelinStar").text)
 	if resp == False or len(localidades) == 0:
 		if local.find("michelinStar").text != None:
-			localidades.append([local.find("municipality").text,int(local.find("michelinStar").text)])
+			localidades.append([local.find("locality").text,int(local.find("michelinStar").text)])
 		else:
-			localidades.append([local.find("municipality").text,0])
+			localidades.append([local.find("locality").text,0])
 
 #Mostramos la lista de localidades y sus respectivas estrellas Michelín.
 for loc in localidades:
 	if loc[1] != 0:
 		print loc[0] + " - " + str(loc[1]) + " estrellas Michelín"
 
-print " ------------------------------------------------------------------------ "
+cont = raw_input("Presiona enter para continuar.")
+print " --------------------------------------------------------------- "
+
 #Busca cuales son los locales recomendados por guías turísticas e indica si estos tienen bodega privada o no.
 for local in locales:
 	if local.find("room").text != None:
@@ -65,4 +67,33 @@ for local in locales:
 			print "El local " + str(local.find("documentName").text) + " dispone de habitaciones pero no de bodega propia."
 
 #JSON
-#
+import json
+f=open("alojamientos.json","r")
+doc = json.load(f)
+hotel = raw_input("Dame el nombre de un alojamiento: ")
+
+print " --------------------------------------------------------------- "
+
+#Dado el nombre de un hotel, mostrar su nombre, dirección, web y descripción en el siguiente formato:
+#HOTEL: “NOMBRE HOTEL”
+#SITUADA EN LA “LOCALIDAD” DE DONDE SEA EN LA CALLE/AVENIDA/PLAZA”
+#“DESCRIPCION”
+#VISITA “WEB DEL HOTEL” PARA MÁS INFORMACIÓN.
+for aloj in doc:
+	if aloj["documentName"] == hotel:
+		print "HOTEL: " + aloj["documentName"]
+		print "SITUADA EN LA LOCALIDAD DE " + aloj["locality"] + " EN LA DIRECCION " + aloj["address"]
+		if aloj["documentDescription"] != None:
+			print aloj["documentDescription"]
+		else:
+			print aloj["turismDescription"]
+		if aloj["web"] != None:
+			print "VISITA " + aloj["web"] + " PARA MAS INFORMACION"
+
+#Dado el número de estrellas concreto, mostrar en el formato anterior los hoteles ordenados alfabéticamente por la localidad en la que se encuentran.
+estrellas = int(raw_input("Dame un número de estrellas: "))
+hoteles = []
+#Almacenamos los hoteles que coinciden con la categoría de entrada
+for aloj in doc:
+	if int(aloj["category"]) == estrellas:
+		hoteles.append(aloj)
