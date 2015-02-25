3053b190231b24abad35e3a23bcf43b2087b71a6
Mariano Triguero <mariano2455@hotmail.com>
2015-02-25 21:31:40 +0100

    Último ejercicio terminado, mejorada la apariencia en ejecución, proyecto finalizado.

--- segundaeva.py
+++ segundaeva.py
@@ -15,7 +15,7 @@ for local in locales:
 	if local.find("municipality").text == localidad:
 		print str(local.find("documentName").text) + " - " + str(local.find("phoneNumber").text) + " - " + str(local.find("email").text)
 
-pausa=raw_input("\n --------------------------------------------------------- \n")
+pausa=raw_input("\n --------------Presiona enter para continuar------------------ \n")
 #Muestra solamente los locales que no tengan acceso para discapacitados dado un tipo de local y un dominio de correo electrónico.
 print "Se van a mostrar los locales que no tienen acceso para discapacitados introduciendo un tipo de local y la parte de un dominio de correo electrónico.\n"
 #De esta manera obtengo todos los tipos de locales que hay para elegir
@@ -26,14 +26,14 @@ for local in locales:
 		print tipo
 		tiposrestaurante.append(tipo)
 
-busqueda = raw_input("¿Qué tipo quieres buscar?\n")
-dominio = raw_input("Con qué dominio de correo quieres buscar?\n")#@loquesea.loquesea
+busqueda = raw_input("\n¿Qué tipo quieres buscar?\n").capitalize()
+dominio = raw_input("Con qué dominio de correo quieres buscar?\n").lower()#@loquesea.loquesea
 #Con esto buscamos el local indicándole el tipo y el dominio
 for local in locales:
 	if local.find("restorationType").text == busqueda and dominio in str(local.find("email").text) and local.find("accesibility").text == None:
 		print local.find("documentName").text
 
-pausa=raw_input("\n --------------------------------------------------------- \n")
+pausa=raw_input("\n --------------Presiona enter para continuar------------------ \n")
 #Cuenta cuantos locales con estrellas Michelín hay por cada población.
 print "Se mostrarán cuantos locales con estrellas Michelín hay por cada población.\n"
 localidades = []
@@ -57,11 +57,10 @@ for loc in localidades:
 	if loc[1] != 0:
 		print loc[0] + " - " + str(loc[1]) + " estrellas Michelín"
 
-cont = raw_input("Presiona enter para continuar.")
-pausa=raw_input("\n --------------------------------------------------------- \n")
+pausa=raw_input("\n --------------Presiona enter para continuar------------------ \n")
 
 #Busca cuales son los locales recomendados por guías turísticas e indica si estos tienen bodega privada o no.
-print "Se mostrará a continuación los locales recomendados por guías turísticas y se mostrará si tienen bodega privada o no\n"
+pausa = raw_input("Se mostrará a continuación los locales recomendados por guías turísticas y se mostrará si tienen bodega privada o no. Pulsa enter para continuar")
 for local in locales:
 	if local.find("room").text != None:
 		if local.find("bodega").text != None:
@@ -69,19 +68,21 @@ for local in locales:
 		else:
 			print "El local " + str(local.find("documentName").text) + " dispone de habitaciones pero no de bodega propia."
 
-pausa=raw_input("\n --------------------------JSON----------------------------- \n")
+pausa=raw_input("\n --------------Presiona enter para continuar------------------ \n")
+print "\n --------------------------JSON----------------------------- \n"
 #JSON
 import json
 f=open("alojamientos.json","r")
 doc1 = json.load(f)
-hotel = raw_input("Dame el nombre de un alojamiento: ")
 
 #Dado el nombre de un hotel, mostrar su nombre, dirección, web y descripción en el siguiente formato:
 #HOTEL: “NOMBRE HOTEL”
 #SITUADA EN LA “LOCALIDAD” DE DONDE SEA EN LA CALLE/AVENIDA/PLAZA”
 #“DESCRIPCION”
 #VISITA “WEB DEL HOTEL” PARA MÁS INFORMACIÓN.
+
 print "Introduce el nombre de un hotel y se mostrará información relevante sobre el mismo.\n"
+hotel = raw_input("Dame el nombre de un alojamiento: ").upper()
 for aloj in doc1:
 	if aloj["documentName"] == hotel:
 		print "HOTEL: " + aloj["documentName"]
@@ -93,11 +94,11 @@ for aloj in doc1:
 		if aloj["web"] != None:
 			print "VISITA " + aloj["web"] + " PARA MAS INFORMACION"
 
-pausa=raw_input("\n --------------------------------------------------------- \n")
+pausa=raw_input("\n --------------Presiona enter para continuar------------------ \n")
 
 #Dado el número de estrellas concreto, mostrar en el formato anterior los hoteles ordenados alfabéticamente por la localidad
 #en la que se encuentran.
-print "Cuando introduzcas un número de estrellas concreto, se mostraran los hoteles ordenados alfabéticamente en el formato del apartado anterior.\n"
+print "Cuando introduzcas un número de estrellas concreto, se mostraran los hoteles ordenados alfabéticamente por localidad en el formato del apartado anterior.\n"
 estrellas = str(raw_input("Dame un número de estrellas: "))
 hoteles = []
 #Almacenamos los hoteles que coinciden con la categoría de entrada
@@ -123,7 +124,7 @@ for ciudad in sorted(ciudades):
 				print "VISITA " + hot["web"] + " PARA MAS INFORMACION\n\
 				\n"
 
-pausa=raw_input("\n --------------------------------------------------------- \n")
+pausa=raw_input("\n --------------Presiona enter para continuar------------------ \n")
 
 #Necesitamos saber cual es el ranking de las mejores ciudades para hacer surf. Para ello contaremos las ciudades que tengan 
 #más alojamientos con posibilidad de hacer surf y las ordenaremos en orden descendente.
@@ -149,14 +150,15 @@ ciudadsurfsorted = OrderedDict(sorted(ciudadsurf.items(), key=lambda t: t[1], re
 for x in ciudadsurfsorted:
 	print x + " - " + str(ciudadsurfsorted[x]) + " alojamientos habilitados para surf."
 
-pausa=raw_input("\n --------------------------------------------------------- \n")
+pausa=raw_input("\n --------------Presiona enter para continuar------------------ \n")
 
 #XML + JSON
 
+print "\n ------------------------XML + JSON--------------------------- \n"
 #Dado el nombre de un alojamiento, muestra los locales situados en la misma localidad que el alojamiento.
 print "Se dará el nombre de un alojamiento y acontinuación se mostrarán los restaurantes de la misma localidad.\n"
 #Obtenemos la ciudad de alojamiento
-entrada = raw_input("Dame el nombre de un alojamiento: ")
+entrada = raw_input("Dame el nombre de un alojamiento: ").upper()
 for x in doc1:
 	if str(x["documentName"]) == entrada:
 		objetivo = str(x["municipality"])
@@ -171,7 +173,7 @@ for local in locales:
 for rest in restaurantes:
 	print rest.find("documentName").text
 
-pausa=raw_input("\n --------------------------------------------------------- \n")
+pausa=raw_input("\n --------------Presiona enter para continuar------------------ \n")
 
 #Muestra 5 posibles combinaciones de alojamiento y restaurante para discapacitados.
 print "Se mostrarán cinco combinaciones aleatorias de alojamiento y restaurante para discapacitados.\n"
@@ -191,7 +193,7 @@ for local in locales:
 for x in xrange(0,5):
 	print "Restaurante " + str(restdisc[randint(0,len(alojdisc) -1)].find("documentName").text) + " y alojamiento " + str(alojdisc[randint(0,len(alojdisc) - 1)]["documentName"])
 
-pausa=raw_input("\n --------------------------------------------------------- \n")
+pausa=raw_input("\n --------------Presiona enter para continuar------------------ \n")
 		
 #¿Cuántos alojamientos y restaurantes poseen la misma marca? Muestra solamente las marcas existentes
 #en ambos archivos. Además debes de almacenar las marcas que no coincidan para después mostrarlas con el formato:
@@ -200,23 +202,39 @@ pausa=raw_input("\n --------------------------------------------------------- \n
 
 #Buscamos las marcas en la lista de alojamientos
 print "Se contarán la cantidad de marcas en ambos archivos(la segunda parte del archivo no esta disponible aún).\n"
-marcas = {}
+marcasaloj = {}
 for aloj in doc1:
-	if str(aloj["marks"]) not in marcas and aloj["marks"] != None :
-		marcas[str(aloj["marks"])] = 1
+	if str(aloj["marks"]) not in marcasaloj and aloj["marks"] != None :
+		marcasaloj[str(aloj["marks"])] = 1
 	elif aloj["marks"] != None:
-		marcas[str(aloj["marks"])] = marcas[str(aloj["marks"])] + 1	
+		marcasaloj[str(aloj["marks"])] = marcasaloj[str(aloj["marks"])] + 1	
 #Buscamos las marcas en la lista de restaurantes
+marcasloc = {}
 for local in locales:
-	if str(local.find("marks").text) not in marcas and local.find("marks").text != None:
-		marcas[str(local.find("marks").text)] = 1
+	if str(local.find("marks").text) not in marcasloc and local.find("marks").text != None:
+		marcasloc[str(local.find("marks").text)] = 1
 	elif local.find("marks").text != None:
-		marcas[str(local.find("marks").text)] = marcas[str(local.find("marks").text)] + 1
+		marcasloc[str(local.find("marks").text)] = marcasloc[str(local.find("marks").text)] + 1
 
-#Mostramos las marcas y la cantidad de repeticiones
-for x in marcas:
-	print "Marca: " + x + " se repite " + str(marcas[x]) + " veces."
+#Mostramos las marcas que coinciden y la cantidad de repeticiones
+marcasind = []
+for x in marcasaloj:
+	if str(x) in marcasloc:
+		print "La marca \"" + str(x) + "\" se encuentra en ambos archivos y se repite " + str(marcasaloj[x] + marcasloc[x]) + " veces."
+#Las que no las agregamos a una lista independiente	
+	elif str(x) not in marcasloc and str(x) != "":
+		marcasind.append(str(x))
 
+print "\nLas marcas que no están en ambos archivos son: \n"
 
+#Buscamos las marcas que están solo en alojamientos
+for x in marcasind:
+	for aloj in doc1:
+		if str(aloj["marks"]) == x:
+			print "La marca \"" + x + "\" la posee el alojamiento " + aloj["documentName"]
 
-
+#Buscamos las marcas que están solo en restaurantes
+for x in marcasind:
+	for local in locales:
+		if str(local.find("marks").text) == str(x):
+			print "La marca \"" + x + "\" la posee el restaurante " + local.find("documentName").text
